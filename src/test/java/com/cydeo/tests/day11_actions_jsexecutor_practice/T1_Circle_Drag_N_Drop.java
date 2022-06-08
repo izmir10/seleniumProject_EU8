f930229eed5f1c45a38628094d5458cdd9aa9ff7
Enis <87227909+izmir10@users.noreply.github.com>
2022-06-08 11:12:40 +0200

    day11 actions_jsexecutor_practice

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T1_Circle_Drag_N_Drop {
+
+    @Test
+    public void drag_and_drop_test(){
+
+        //TC #: Drag and drop
+        //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+
+        //Locate and accept cookies button
+        //BrowserUtils.sleep(2);
+        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
+        acceptCookiesButton.click();
+
+        //Locate small and big circles
+        WebElement smallCircle = Driver.getDriver().findElement(By.xpath("//div[@id='draggable']"));
+        WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
+
+        //2. Drag and drop the small circle to bigger circle.
+        Actions actions = new Actions(Driver.getDriver());
+        BrowserUtils.sleep(2);
+//        actions.dragAndDrop(smallCircle, bigCircle).perform();
+        actions.clickAndHold(smallCircle)
+                .pause(2000)
+                .moveToElement(bigCircle)
+                .pause(2000)
+                .release()
+                .perform();
+
+        //3. Assert:
+        //-Text in big circle changed to: “You did great!”
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText, expectedBigCircleText);
+
+
+    }
+
+}
