567304e6e853f77eddd25a76faaed697188006f1
Enis <87227909+izmir10@users.noreply.github.com>
2022-06-09 20:50:41 +0200

    day12 WebDriverWaitPractices

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -3,11 +3,24 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractices {
 
     LibraryLoginPage libraryLoginPage;
+
+    @BeforeMethod
+    public void setupMethod(){
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPage = new LibraryLoginPage();
+    }
+
+    @AfterMethod
+    public void teardownMethod(){
+        Driver.closeDriver();
+    }
 
     @Test
     public void required_field_error_message_test(){
@@ -15,19 +28,19 @@ public class POMPractices {
         //TC #1: Required field error message test
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
+
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        libraryLoginPage = new LibraryLoginPage();
+
         libraryLoginPage.signInButton.click();
 
         //5- Verify expected error is displayed:
         //Expected: This field is required
-        String expectedErrorMessage = "This field is required.";
+
         Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
 
-        Driver.closeDriver();
+
 
     }
 
@@ -36,19 +49,35 @@ public class POMPractices {
         //TC #2: Invalid email format error message test
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
-        Driver.getDriver().get("https://library1.cydeo.com");
 
         //3- Enter invalid email format
-        libraryLoginPage = new LibraryLoginPage();
 
-        libraryLoginPage.inputUsername.sendKeys("tempmail.email.com");
+        libraryLoginPage.inputUsername.sendKeys("invalid");
         libraryLoginPage.signInButton.click();
 
         //4- Verify expected error is displayed:
         //Expected: Please enter a valid email address.
         Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
 
-        Driver.closeDriver();
+    }
+
+    @Test
+    public void library_negative_login_test(){
+        //TC #3: Library negative login
+        //1- Open a Chrome browser
+        //2- Go to: https://library1.cydeo.com
+
+        //3- Enter incorrect username or incorrect password
+
+        libraryLoginPage.inputUsername.sendKeys("invalid@email.com");
+        libraryLoginPage.inputPassword.sendKeys("invalid");
+
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify title expected error is displayed:
+        //Expected: Sorry, Wrong Email or Password
+
+        Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
 
     }
 
